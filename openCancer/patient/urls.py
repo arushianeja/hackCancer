f919e3a5c9cee8ecb61dead8d388ac50c3bb9f24
Morten Kallberg <mkallberg@illumina.com>
2015-10-10 04:55:31 -0700

    single user query

--- openCancer/patient/urls.py
+++ openCancer/patient/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from patient import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.get_all_users, name='get_all_users'),
 ]
--- openCancer/patient/views.py
+++ openCancer/patient/views.py
@@ -10,7 +10,12 @@ from django.core import serializers
 #     html = "<html><body>It is now %s.</body></html>" % now
 #     return HttpResponse(html)
 
-def index(request):
-    users = User.objects.all()
+def get_all_patients(request):
+    users = User.objects.filter(is_patient=1)
+    data = serializers.serialize("json", users)
+    return HttpResponse(data, content_type='application/json')
+
+def get_single_user(request,id):
+    users = User.objects.filter(id=1)
     data = serializers.serialize("json", users)
     return HttpResponse(data, content_type='application/json')
